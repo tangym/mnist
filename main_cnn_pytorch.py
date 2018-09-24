318cd486a22638df786df937d89ba79d18682cad
tangym <ym.tang@qq.com>
2018-09-24 17:57:19 -0400

    learn how to use ignite framework to train torch models

--- main_cnn_pytorch.py
+++ main_cnn_pytorch.py
@@ -9,14 +9,18 @@ import torch
 from torch.autograd import Variable
 import numpy as np
 import torch.nn as nn
-from torchvision import datasets, transforms
+from torchvision import datasets, models, transforms
+from torch.utils.data import TensorDataset, DataLoader
+from ignite.engine import Engine, create_supervised_trainer, Events
+from ignite.handlers import Timer
+from torch.utils.data import Dataset, DataLoader
 
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import LabelBinarizer
 
 import pylab as plt
-
+from tqdm import tqdm
 
 #%%
 #dtype = torch.float
@@ -49,21 +53,47 @@ class C2F3(nn.Module):
         out = self.fc(out)
         return out 
 
-
 #%%
+def batches(X, batch_size=1000):
+    for i in range(0, len(X), batch_size):
+        yield X[i : i + batch_size]
+
+#%% Prepare train/test data
 data = pd.read_csv('train.csv')        # 28*28
 test = pd.read_csv('test.csv')
 
-#%%
 X = data[[col for col in data.columns if col.startswith('pixel')]].values
 binarizer = LabelBinarizer()
 y = binarizer.fit_transform(data['label'].values)
 X_test = test[[col for col in data.columns if col.startswith('pixel')]].values
 n_classes = len(data['label'].unique())
 
+X = torch.tensor(X.reshape(len(X), 1, 28, 28), dtype=torch.float)
+y = torch.tensor(y, dtype=torch.int).float()
+X_test = torch.tensor(X_test.reshape(len(X_test), 1, 28, 28), dtype=torch.float)
+
 del data, test
 
 #%%
+class MNISTDataset(Dataset):
+    def __init__(self, filename):
+        data = pd.read_csv(filename)
+        self.X = data[[col for col in data.columns if col.startswith('pixel')]].values
+        self.y = LabelBinarizer().fit_transform(data['label'].values)
+        self.X = torch.tensor(self.X.reshape(len(self.X), 1, 28, 28), dtype=torch.float)
+        self.y = torch.tensor(self.y, dtype=torch.int).float()
+
+    def __len__(self):
+        return len(self.X)
+    
+    def __getitem__(self, idx):
+        return (self.X[idx], self.y[idx])
+
+#%%
+dataset = MNISTDataset('train_small.csv')
+
+
+#%% Initialize model
 model = C2F3(10)
 
 criterion = nn.MSELoss(reduction='sum')
@@ -71,23 +101,53 @@ criterion = nn.MSELoss(reduction='sum')
 #optimizer = torch.optim.SGD(model.parameters(), lr=1e-4)
 optimizer = torch.optim.Adadelta(model.parameters(), lr=1e-4)
 
-#%%
-n_samples = 800
-tX = torch.tensor(X[:n_samples].reshape(n_samples, 1, 28, 28), dtype=torch.float)
-ty = torch.tensor(y[:n_samples], dtype=torch.int)
 
 #%%
 for t in range(10):
-    y_pred = model(tX)
-    loss = criterion(y_pred, ty.float())
-    print(t, loss.item())
-    optimizer.zero_grad()
-    loss.backward()
-    optimizer.step()
+    for batch_X, batch_y in zip(batches(X), batches(y)):
+        y_pred = model(batch_X)
+        loss = criterion(y_pred, batch_y)
+        #print(t, loss.item())
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
 
 #%%
+engine = create_supervised_trainer(model, optimizer, criterion)
+
+@engine.on(Events.ITERATION_COMPLETED)
+def log_training_results(trainer):
+    print(trainer.state.epoch, timer.value())
 
 
 
 #%%
+@engine.on(Events.ITERATION_COMPLETED)
+def log_training_loss(trainer):
+    print("Epoch[{}] Loss: {:.2f}".format(trainer.state.epoch, trainer.state.output))
+
+@engine.on(Events.EPOCH_COMPLETED)
+def log_training_results(trainer):
+    evaluator.run(train_loader)
+    metrics = evaluator.state.metrics
+    print("Training Results - Epoch: {}  Avg accuracy: {:.2f} Avg loss: {:.2f}"
+          .format(trainer.state.epoch, metrics['accuracy'], metrics['nll']))
+
+@engine.on(Events.EPOCH_COMPLETED)
+def log_validation_results(trainer):
+    evaluator.run(val_loader)
+    metrics = evaluator.state.metrics
+    print("Validation Results - Epoch: {}  Avg accuracy: {:.2f} Avg loss: {:.2f}"
+          .format(trainer.state.epoch, metrics['accuracy'], metrics['nll']))
+
+#%%
+engine.run(zip(batches(X), batches(y)), max_epochs=3)
+
+engine.run(DataLoader(dataset, batch_size=1000), max_epochs=2)
+
+
+timer = Timer(average=True)
+timer.attach(engine, start=Events.EPOCH_STARTED, resume=Events.ITERATION_STARTED,
+             pause=Events.ITERATION_COMPLETED, step=Events.ITERATION_COMPLETED)
+
 
